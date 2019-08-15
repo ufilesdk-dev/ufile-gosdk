e8e0c846875aedb4995feabd07315665383c4716
chen <jiolin.chen@ucloud.cn>
2019-08-15 15:35:42 +0800

    opt:
    "新增支持Rename、Copy功能"
    
    Signed-off-by: chen <jiolin.chen@ucloud.cn>

--- example/demo_file.go
+++ example/demo_file.go
@@ -15,6 +15,8 @@ const (
 	asyncmput
 )
 
+var srcBucketName string
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 	if _, err := os.Stat(helper.FakeSmallFilePath); os.IsNotExist(err) {
@@ -32,6 +34,9 @@ func main() {
 		panic(err.Error())
 	}
 
+	//可以替换为自定义源bucketName
+	srcBucketName = config.BucketName
+
 	var fileKey string
 	fileKey = helper.GenerateUniqKey()
 	scheduleUploadhelper(helper.FakeSmallFilePath, fileKey, putUpload, req)
@@ -42,6 +47,7 @@ func main() {
 	scheduleUploadhelper(helper.FakeBigFilePath, fileKey, mput, req)
 	fileKey = helper.GenerateUniqKey()
 	scheduleUploadhelper(helper.FakeBigFilePath, fileKey, asyncmput, req)
+
 }
 
 func scheduleUploadhelper(filePath, keyName string, uploadType int, req *ufsdk.UFileRequest) {
@@ -84,11 +90,29 @@ func scheduleUploadhelper(filePath, keyName string, uploadType int, req *ufsdk.U
 	if err != nil {
 		log.Println("文件秒传失败，错误信息为：", err.Error())
 	} else {
-		log.Printf("秒传文件返回的信息是：%s\n", req.LastResponseBody)
+		log.Printf("操作成功，秒传文件返回的信息是：%s\n", req.LastResponseBody)
+	}
+
+	log.Println("正在重命名文件...")
+	newKeyName := "test_newKey"
+	err = req.Rename(keyName, newKeyName, "")
+	if err != nil {
+		log.Println("文件重命名失败，错误信息为：", err.Error())
+	} else {
+		log.Printf("操作成功，重命名文件返回的信息是：%s\n", req.LastResponseBody)
+	}
+
+	log.Println("正在拷贝文件...")
+	dstKeyName := "test_dstKey"
+	err = req.Copy(dstKeyName, srcBucketName, newKeyName)
+	if err != nil {
+		log.Println("文件拷贝失败，错误信息为：", err.Error())
+	} else {
+		log.Printf("操作成功，拷贝文件返回的信息是：%s\n", req.LastResponseBody)
 	}
 
 	log.Println("正在获取文件列表...")
-	list, err := req.PrefixFileList(keyName, "", 10)
+	list, err := req.PrefixFileList(newKeyName, "", 10)
 	if err != nil {
 		log.Println("获取文件列表失败，错误信息为：", err.Error())
 		return
@@ -96,7 +120,7 @@ func scheduleUploadhelper(filePath, keyName string, uploadType int, req *ufsdk.U
 	log.Printf("获取文件列表返回的信息是：\n%s\n", list)
 
 	log.Println("正在删除刚刚上传的文件")
-	err = req.DeleteFile(keyName)
+	err = req.DeleteFile(newKeyName)
 	if err != nil {
 		log.Println("删除文件失败，错误信息为：", err.Error())
 		return
