e3423a8eeafa40252f814527ffd96a7df52a7018
xiaobo.chen <xiaobo.chen@ucloud.cn>
2022-12-22 14:01:00 +0800

    [fix]revert file.go

--- file.go
+++ file.go
@@ -595,39 +595,3 @@ func (u *UFileRequest) ListObjects(prefix, marker, delimiter string, maxkeys int
 	err = json.Unmarshal(u.LastResponseBody, &list)
 	return
 }
-
-// SetMetaRequest  用于 设置元数据中的requestBody
-type SetMetaRequest struct {
-	Op    string `json:"op"`
-	MetaK string `json:"metak"`
-	MetaV string `json:"metav"`
-}
-
-// SetMimeType 设置key的mime类型
-//key 对象的key
-//mimeType 要设置的mime类型
-func (u *UFileRequest) SetMimeType(key, mimeType string) (err error) {
-	//构建SetMeta请求体
-	setMimeTypeRequest := SetMetaRequest{
-		Op:    "set",
-		MetaK: "mimetype",
-		MetaV: mimeType,
-	}
-	requestBody, err := json.Marshal(setMimeTypeRequest)
-	if err != nil {
-		return err
-	}
-	h := make(http.Header)
-	h.Add("Content-Type", "application/json;charset=UTF-8")
-	//生成签名
-	authorization := u.Auth.Authorization("POST", u.BucketName, key, h)
-	reqUrl := u.genFileURL(key) + "?opmeta"
-	req, err := http.NewRequest("POST", reqUrl, bytes.NewBuffer(requestBody))
-	if err != nil {
-		return err
-	}
-	req.Header.Add("Content-Length", strconv.Itoa(len(requestBody)))
-	req.Header.Add("Authorization", authorization)
-	req.Header.Add("Content-Type", "application/json;charset=UTF-8")
-	return u.request(req)
-}
