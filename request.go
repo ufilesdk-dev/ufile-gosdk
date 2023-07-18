234fa59a0ac66f7bbe5f3e7cb1b7cfb0ad5b305b
xiaobo.chen <xiaobo.chen@ucloud.cn>
2023-07-18 18:17:33 +0800

    [feature]update version

--- request.go
+++ request.go
@@ -11,16 +11,15 @@ import (
 	"strings"
 )
 
-//UFileRequest SDK 主要的 request 模块。本 SDK 遵从以下原则：
+// UFileRequest SDK 主要的 request 模块。本 SDK 遵从以下原则：
 //
-//1.接口尽可能简洁，隐藏所有复杂实现。
+// 1.接口尽可能简洁，隐藏所有复杂实现。
 //
-//2.本 SDK 主要的作用是封装 HTTP 请求，不做过多其他的封装（如 HTTP Body 序列化，详细的错误检查）。
+// 2.本 SDK 主要的作用是封装 HTTP 请求，不做过多其他的封装（如 HTTP Body 序列化，详细的错误检查）。
 //
-//3.只简单封装 HTTP 请求所需要的参数，给接口使用者提供所有原生的 HTTP response header,body,status code 返回，以便排错。
+// 3.只简单封装 HTTP 请求所需要的参数，给接口使用者提供所有原生的 HTTP response header,body,status code 返回，以便排错。
 //
-//4.远端请求返回值统一返回一个 error，如果为 nil 表示无错。LastResponseStatus，LastResponseHeader，LastResponseBody 可以查看具体的 HTTP 返回信息（）。如果你想少敲几行代码可以直接调用 DumpResponse(true) 查看详细返回。
-//
+// 4.远端请求返回值统一返回一个 error，如果为 nil 表示无错。LastResponseStatus，LastResponseHeader，LastResponseBody 可以查看具体的 HTTP 返回信息（）。如果你想少敲几行代码可以直接调用 DumpResponse(true) 查看详细返回。
 type UFileRequest struct {
 	Auth          Auth
 	BucketName    string
@@ -37,11 +36,11 @@ type UFileRequest struct {
 	lastResponse       *http.Response
 }
 
-//NewFileRequest 创建一个用于管理文件的 request，管理文件的 url 与 管理 bucket 接口不一样，
-//请将 bucket 和文件管理所需要的分开，NewUBucketRequest 是用来管理 bucket 的。
-//Request 创建后的 instance 不是线程安全的，如果你需要做并发的操作，请创建多个 UFileRequest。
-//config 参数里面包含了公私钥，以及其他必填的参数。详情见 config 相关文档。
-//client 这里你可以传空，会使用默认的 http.Client。如果你需要设置超时以及一些其他相关的网络配置选项请传入一个自定义的 client。
+// NewFileRequest 创建一个用于管理文件的 request，管理文件的 url 与 管理 bucket 接口不一样，
+// 请将 bucket 和文件管理所需要的分开，NewUBucketRequest 是用来管理 bucket 的。
+// Request 创建后的 instance 不是线程安全的，如果你需要做并发的操作，请创建多个 UFileRequest。
+// config 参数里面包含了公私钥，以及其他必填的参数。详情见 config 相关文档。
+// client 这里你可以传空，会使用默认的 http.Client。如果你需要设置超时以及一些其他相关的网络配置选项请传入一个自定义的 client。
 func NewFileRequest(config *Config, client *http.Client) (*UFileRequest, error) {
 	config.BucketName = strings.TrimSpace(config.BucketName)
 	config.FileHost = strings.TrimSpace(config.FileHost)
@@ -65,11 +64,11 @@ func NewFileRequest(config *Config, client *http.Client) (*UFileRequest, error) 
 	return req, nil
 }
 
-//NewFileRequestWithHeader 创建一个可携带用户自定义http请求头的 request
-//Request 创建后的 instance 不是线程安全的，如果你需要做并发的操作，请创建多个 UFileRequest。
-//config 参数里面包含了公私钥，以及其他必填的参数。详情见 config 相关文档。
-//header 自定义http请求头
-//client 这里你可以传空，会使用默认的 http.Client。如果你需要设置超时以及一些其他相关的网络配置选项请传入一个自定义的 client。
+// NewFileRequestWithHeader 创建一个可携带用户自定义http请求头的 request
+// Request 创建后的 instance 不是线程安全的，如果你需要做并发的操作，请创建多个 UFileRequest。
+// config 参数里面包含了公私钥，以及其他必填的参数。详情见 config 相关文档。
+// header 自定义http请求头
+// client 这里你可以传空，会使用默认的 http.Client。如果你需要设置超时以及一些其他相关的网络配置选项请传入一个自定义的 client。
 func NewFileRequestWithHeader(config *Config, header http.Header, client *http.Client) (*UFileRequest, error) {
 	req, err := NewFileRequest(config, client)
 	if err != nil {
@@ -79,9 +78,9 @@ func NewFileRequestWithHeader(config *Config, header http.Header, client *http.C
 	return req, nil
 }
 
-//NewBucketRequest 创建一个用于管理 bucket 的 request。
-//注意：不要拿它去调用文件管理的 request，我文件管理和 bucket 管理接口放到一个 request 里面的目的就是让接口更统一，代码更清晰，简洁。
-//config 参数里面包含了公私钥，以及其他必填的参数。详情见 config 相关文档。
+// NewBucketRequest 创建一个用于管理 bucket 的 request。
+// 注意：不要拿它去调用文件管理的 request，我文件管理和 bucket 管理接口放到一个 request 里面的目的就是让接口更统一，代码更清晰，简洁。
+// config 参数里面包含了公私钥，以及其他必填的参数。详情见 config 相关文档。
 func NewBucketRequest(config *Config, client *http.Client) (*UFileRequest, error) {
 	req := newRequest(config.PublicKey, config.PrivateKey, "", config.BucketHost, client)
 	req.verifyUploadMD5 = config.VerifyUploadMD5
@@ -91,8 +90,8 @@ func NewBucketRequest(config *Config, client *http.Client) (*UFileRequest, error
 	return req, nil
 }
 
-//DumpResponse dump 当前请求的返回结果，里面有一个 print 函数，会把 body,header,status code 直接输出到 stdout。
-//如果你需要 Dump 到其他的地方，直接拿返回值即可。
+// DumpResponse dump 当前请求的返回结果，里面有一个 print 函数，会把 body,header,status code 直接输出到 stdout。
+// 如果你需要 Dump 到其他的地方，直接拿返回值即可。
 func (u *UFileRequest) DumpResponse(isDumpBody bool) []byte {
 	var b bytes.Buffer
 	if u.lastResponse == nil {
@@ -167,7 +166,7 @@ func (u *UFileRequest) request(req *http.Request) error {
 }
 
 func (u *UFileRequest) requestWithResp(req *http.Request) (resp *http.Response, err error) {
-	req.Header.Set("User-Agent", "UFileGoSDK/2.02")
+	req.Header.Set("User-Agent", "UFileGoSDK/1.0.4")
 
 	// cover current request headers with user defined headers
 	if u.RequestHeader != nil {
